5831dc03fbb5c68acf521c2d907af736f50c3223
minge44 <mingemedia@gmail.com>
2017-06-29 02:43:41 -0400

    second commit

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const express = require('express');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
 
-const data = require("./data.js");
+const data = require("./app/data.js");
 
 const app = express();
-
+app.use(express.static('public'));
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.get('/', function (req, res){
+  app.get('/directory/:username', function(req, res) {
+    // return user profile
+    console.log('access username:', req.params.username);
+    var userProfile = robots.users.find(function(x) {
+      return x.username === req.params.username
+    });
+  // console.log(userProfile);
+    res.render('user.mustache', userProfile);
+  });
 
 
-
- res.render('index', { img: data.users[0].avatar })
+ res.render('index', {users:data.users})
 });
 
 app.listen(3000, function () {
